d256697d176b4fecea406094b26307d87e9bbbec
Mo18012 <mohittest903@gmail.com>
2025-10-07 11:44:12 +0530

    fix

--- app/javascript/frontend/page.jsx
+++ app/javascript/frontend/page.jsx
@@ -27,7 +27,6 @@ import HideBottomNavCard from './BottomNavCard';
 import FirebaseNotificationCard from './NotificationCard';
 import HapticFeedbackCard from './HapticFeedbackCard';
 import AdMobCard from './admob';
-import AdMobCard from './nativcontact';
 import NativeContactsCard from './Nativecontact';
 
 export default function FrontendPage() {
